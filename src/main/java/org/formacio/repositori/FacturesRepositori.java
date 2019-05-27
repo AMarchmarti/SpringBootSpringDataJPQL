764527900860251736bd8f5688d830e88b17a054
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-05-27 21:11:42 +0200

    Add method, of derivatives queries, findByClientNom, run perfectly

--- src/main/java/org/formacio/repositori/FacturesRepositori.java
+++ src/main/java/org/formacio/repositori/FacturesRepositori.java
@@ -6,10 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FacturesRepositori extends CrudRepository<Factura, Long> {
 
 	@Query("select sum(linia.total) from Factura f join f.linies linia where f.client.nom = ?1")
 	public Number totalClient(String client);
+
+	List<Factura> findByClientNom(String nom);
 	
 }
